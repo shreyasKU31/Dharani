e269c20ed04f8c5fa628cb268e6143a404195c04
shreyasKU31 <shreyasku31@gmail.com>
2025-09-23 16:31:34 +0530

    AI model is ready

--- crop_predictor.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import joblib
-
-def predict_crop(n, p, k, temperature, humidity, ph, rainfall):
-    model = joblib.load('crop_model.pkl')
-    input_data = pd.DataFrame([[n, p, k, temperature, humidity, ph, rainfall]],
-                              columns=['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall'])
-    return model.predict(input_data)[0]
